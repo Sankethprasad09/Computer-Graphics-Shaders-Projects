67131335e3957b7069d2f7d2ad58a0b06cefc4c9
Sankethprasad09 <132974227+Sankethprasad09@users.noreply.github.com>
2023-12-27 18:42:34 -0800

    Add files via upload

---
+++ Samplemac/some.cpp
@@ -0,0 +1,71 @@
+// Object Transparency
+ObjectTransparency.AddTimeValue(0.0, 0.0);
+ObjectTransparency.AddTimeValue(2.0, 0.3);
+ObjectTransparency.AddTimeValue(5.0, 0.7);
+ObjectTransparency.AddTimeValue(7.0, 0.9);
+ObjectTransparency.AddTimeValue(10.0, 1.0);
+
+// Light Intensity
+lightIntensity.AddTimeValue(0.0, 0.2);
+lightIntensity.AddTimeValue(2.0, 0.5);
+lightIntensity.AddTimeValue(4.0, 0.8);
+lightIntensity.AddTimeValue(6.0, 0.5);
+lightIntensity.AddTimeValue(8.0, 0.2);
+lightIntensity.AddTimeValue(10.0, 1.0);
+
+// Light Position X
+lightPosX.AddTimeValue(0.0, originalPosX);
+lightPosX.AddTimeValue(2.0, 5.0);
+lightPosX.AddTimeValue(5.0, -5.0);
+lightPosX.AddTimeValue(7.0, 10.0);
+lightPosX.AddTimeValue(10.0, originalPosX);
+
+// Light Position Y
+lightPosY.AddTimeValue(0.0, originalPosY);
+lightPosY.AddTimeValue(2.0, 5.0);
+lightPosY.AddTimeValue(5.0, -5.0);
+lightPosY.AddTimeValue(7.0, 10.0);
+lightPosY.AddTimeValue(10.0, originalPosY);
+
+// Light Position Z
+lightPosZ.AddTimeValue(0.0, originalPosZ);
+lightPosZ.AddTimeValue(2.0, 5.0);
+lightPosZ.AddTimeValue(5.0, -5.0);
+lightPosZ.AddTimeValue(7.0, 10.0);
+lightPosZ.AddTimeValue(10.0, originalPosZ);
+
+// Light Color Red
+lightColorR.AddTimeValue(0.0, 1.0);
+lightColorR.AddTimeValue(1.0, 0.0);
+lightColorR.AddTimeValue(2.0, 1.0);
+lightColorR.AddTimeValue(3.0, 1.0);
+lightColorR.AddTimeValue(4.0, 0.0);
+lightColorR.AddTimeValue(5.0, 1.0);
+lightColorR.AddTimeValue(6.0, 1.0);
+lightColorR.AddTimeValue(7.0, 0.0);
+lightColorR.AddTimeValue(8.0, 1.0);
+lightColorR.AddTimeValue(9.0, 0.0);
+
+// Light Color Green
+lightColorG.AddTimeValue(0.0, 0.0);
+lightColorG.AddTimeValue(1.0, 1.0);
+lightColorG.AddTimeValue(2.0, 0.0);
+lightColorG.AddTimeValue(3.0, 0.0);
+lightColorG.AddTimeValue(4.0, 1.0);
+lightColorG.AddTimeValue(5.0, 0.0);
+lightColorG.AddTimeValue(6.0, 0.0);
+lightColorG.AddTimeValue(7.0, 1.0);
+lightColorG.AddTimeValue(8.0, 0.0);
+lightColorG.AddTimeValue(9.0, 1.0);
+
+// Light Color Blue
+lightColorB.AddTimeValue(0.0, 0.0);
+lightColorB.AddTimeValue(1.0, 0.0);
+lightColorB.AddTimeValue(2.0, 1.0);
+lightColorB.AddTimeValue(3.0, 0.0);
+lightColorB.AddTimeValue(4.0, 0.0);
+lightColorB.AddTimeValue(5.0, 1.0);
+lightColorB.AddTimeValue(6.0, 0.0);
+lightColorB.AddTimeValue(7.0, 0.0);
+lightColorB.AddTimeValue(8.0, 1.0);
+lightColorB.AddTimeValue(9.0, 0.0);
